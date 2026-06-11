667cfeb0341be84ded70e73c01ab8264a3519462
Joseph071 <josephparedes867@gmail.com>
2026-06-11 11:13:02 -0500

    feat: update route model

---
+++ internal/models/ruta.go
@@ -0,0 +1,11 @@
+package models
+
+type Ruta struct {
+	ID         int    `json:"id"`
+	Sector     string `json:"sector"`
+	Destino    string `json:"destino"`
+	HoraSalida string `json:"hora_salida"`
+	Cupos      int    `json:"cupos"`
+	Estado     string `json:"estado"`
+	Conductor  string `json:"conductor"`
+}
