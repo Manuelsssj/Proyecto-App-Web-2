a4459f98678ae72ad14b17383137b1c74e561284
xJoanrole <jose2004romero@gmail.com>
2026-06-11 21:25:40 -0500

    feat: registrar rutas completas de suscripciones

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -15,6 +15,9 @@ func main() {
 	r.Route("/api/v1/suscripciones", func(r chi.Router) {
 		r.Post("/", handlers.CreateSuscripcion)
 		r.Get("/", handlers.GetSuscripciones)
+		r.Get("/{id}", handlers.GetSuscripcion)
+		r.Put("/{id}", handlers.UpdateSuscripcion)
+		r.Delete("/{id}", handlers.DeleteSuscripcion)
 	})
 
 	fmt.Println("Servidor corriendo en http://localhost:8080")
