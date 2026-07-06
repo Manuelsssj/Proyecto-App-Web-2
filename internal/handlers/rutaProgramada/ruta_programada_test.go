6a2140850a0154d075f720e9f53012d7f1903d46
Manuelsssj <magta511@hotmail.com>
2026-07-06 11:30:46 -0500

    demo: pipeline CP3 - Manuel

--- internal/handlers/rutaProgramada/ruta_programada_test.go
+++ internal/handlers/rutaProgramada/ruta_programada_test.go
@@ -138,7 +138,7 @@ func TestRutaProgramadaHandler_CrearRutaConTokenResponde201(t *testing.T) {
 
 	r.ServeHTTP(rec, req)
 
-	if rec.Code != http.StatusCreated {
+	if rec.Code != http.StatusTeapot {
 		t.Fatalf("se esperaba status 201, se obtuvo %d. Respuesta: %s", rec.Code, rec.Body.String())
 	}
 
